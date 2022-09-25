d6cf50be3ddef18e1d4cca3a8a401ecaaf93f1c1
Artem Beletzky <belezky.artem@gmail.com>
2022-09-25 23:31:10 +0300

    add 'format' argument to arguments parser

--- gendiff/cli.py
+++ gendiff/cli.py
@@ -5,4 +5,5 @@ def parse_args():
     parser = argparse.ArgumentParser(description='Compares two configuration files and shows the difference.')
     parser.add_argument('first_file', type=str)
     parser.add_argument('second_file', type=str)
+    parser.add_argument('-f', '--format', action='store_true', help='set format of output')
     return parser.parse_args()
