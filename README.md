6101ebb7c144b706c21534a661c59236a64036b9
Artem Beletzky <belezky.artem@gmail.com>
2022-09-30 09:15:39 +0300

    feat: add Makefile, add badges, add linter, lint code

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/artemBeletzky/python-project-50/workflows/hexlet-check/badge.svg)](https://github.com/artemBeletzky/python-project-50/actions)
+
+### Build and lint:
+None
+
+### Code climate: 
+<a href="https://codeclimate.com/github/artemBeletzky/python-project-50/maintainability"><img src="https://api.codeclimate.com/v1/badges/f91d91ac1599e8defe33/maintainability" /></a>
--- gendiff/cli.py
+++ gendiff/cli.py
@@ -2,8 +2,12 @@ import argparse
 
 
 def parse_args():
-    parser = argparse.ArgumentParser(description='Compares two configuration files and shows the difference.')
-    parser.add_argument('first_file', type=str)
-    parser.add_argument('second_file', type=str)
-    parser.add_argument('-f', '--format', action='store_true', help='set format of output')
+    parser = argparse.ArgumentParser(
+        description="Compares two configuration files and shows the difference."
+    )
+    parser.add_argument("first_file", type=str)
+    parser.add_argument("second_file", type=str)
+    parser.add_argument(
+        "-f", "--format", action="store_true", help="set format of output"
+    )
     return parser.parse_args()
